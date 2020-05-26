d0fb7d4cb171ffc66f1fba7173bb17dcb84eb3f1
Lingda Li <lildmh@gmail.com>
2020-05-25 22:22:36 -0400

    Add multi-GPU support

--- dp/6_get_test.py
+++ dp/6_get_test.py
@@ -1,9 +1,9 @@
 import os
 import numpy as np
 
-data_set_name = "data_spec"
-batchnum = 16 * 16 * 4
-batchsize = 32 * 1024
+data_set_name = "data"
+batchnum = 16 * 16 * 2
+batchsize = 32 * 1024 * 2
 
 data = np.load(data_set_name + "/totalall.npz")
 x = data['x']
@@ -12,4 +12,4 @@ x = data['x']
 x_test = x[batchnum*batchsize:(batchnum+1)*batchsize,]
 #y_test = y[batchnum*batchsize:(batchnum+1)*batchsize,]
 #np.savez_compressed(data_set_name + "/test.npz", x=x_test, y=y_test)
-np.savez_compressed(data_set_name + "/nntest.npz", x=x_test)
+np.savez_compressed(data_set_name + "/test.npz", x=x_test)
