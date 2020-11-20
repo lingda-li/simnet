db90801769779e7c55181851c3eb5c66cc96abc4
Lingda Li <lildmh@gmail.com>
2020-11-20 10:57:13 -0500

    Update testing script

--- ml/test_lat.py
+++ ml/test_lat.py
@@ -160,6 +160,9 @@ for i in range(2):
     print(errs)
 
   print("cycle err: ", np.average(errs[errs != -1]))
+  flat_target = target[:,i:i+1]
+  flat_target = flat_target.ravel()
+  print("presentage err: ", np.sum(errs[errs != -1]) / np.sum(flat_target[errs != -1]))
 
   his = np.histogram(errs, bins=range(-1, 100))
   print(errs[errs != -1].size / errs.size)
