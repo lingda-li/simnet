b831268f8370b983db8e899cb6afb41c4902ea48
Lingda Li <lildmh@gmail.com>
2020-12-11 16:40:20 -0500

    Add CNN4 model

--- ml/models.py
+++ ml/models.py
@@ -240,6 +240,37 @@ class CNN5_F(nn.Module):
         x = self.fc2(x)
         return x
 
+class CNN4_F(nn.Module):
+    def __init__(self, out, ck1, ch1, cs1, cp1, ck2, ch2, cs2, cp2, ck3, ch3, cs3, cp3, ck4, ch4, cs4, cp4, f1):
+        super(CNN4_F, self).__init__()
+        self.conv1 = nn.Conv1d(in_channels=inst_length, out_channels=ch1, kernel_size=ck1, stride=cs1, padding=cp1)
+        self.conv2 = nn.Conv1d(in_channels=ch1, out_channels=ch2, kernel_size=ck2, stride=cs2, padding=cp2)
+        self.conv3 = nn.Conv1d(in_channels=ch2, out_channels=ch3, kernel_size=ck3, stride=cs3, padding=cp3)
+        self.conv4 = nn.Conv1d(in_channels=ch3, out_channels=ch4, kernel_size=ck4, stride=cs4, padding=cp4)
+        self.f1_input = math.floor((context_length + 2 * cp1 - ck1) / cs1 + 1)
+        print(self.f1_input)
+        self.f1_input = math.floor((self.f1_input + 2 * cp2 - ck2) / cs2 + 1)
+        print(self.f1_input)
+        self.f1_input = math.floor((self.f1_input + 2 * cp3 - ck3) / cs3 + 1)
+        print(self.f1_input)
+        self.f1_input = math.floor((self.f1_input + 2 * cp4 - ck4) / cs4 + 1)
+        print(self.f1_input)
+        self.f1_input *= ch4
+        self.f1_input = int(self.f1_input)
+        self.fc1 = nn.Linear(self.f1_input, f1)
+        self.fc2 = nn.Linear(f1, out)
+
+    def forward(self, x):
+        x = x.view(-1, inst_length, context_length)
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = x.view(-1, self.f1_input)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
 class CNN3_P2(nn.Module):
     def __init__(self, out, pc, ck1, ch1, ck2, ch2, ck3, ch3, f1):
         super(CNN3_P2, self).__init__()
