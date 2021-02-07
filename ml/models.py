9a6402f032f9e56b44ceafbb684b6bde4a2bf87f
Lingda Li <lildmh@gmail.com>
2021-02-07 11:47:44 -0500

    Update model cnn3p with batchnorm

--- ml/models.py
+++ ml/models.py
@@ -50,6 +50,19 @@ class Fusion1d(nn.Module):
         x = F.relu(x)
         return x
 
+class Fusion1dS(nn.Module):
+    def __init__(self, out):
+        super(Fusion1dS, self).__init__()
+        self._conv = nn.Conv1d(in_channels=inst_length*2, out_channels=out, kernel_size=1, bias=False)
+        self._cxt_size = context_length
+
+    def forward(self, x):
+        copies = x[:,:,0:1]
+        copies = copies.expand(-1, -1, self._cxt_size - 1)
+        x = torch.cat((x[:,:,1:self._cxt_size],copies),1)
+        x = self._conv(x)
+        return x
+
 class FC2(nn.Module):
     def __init__(self, out, f1):
         super(FC2, self).__init__()
@@ -247,6 +260,36 @@ class CNN3_F_PP_TS(nn.Module):
 
     def forward(self, x):
         x = self._fu(x)
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = x.view(-1, self.f1_input)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+class CNN3_F_P_BN(nn.Module):
+    def __init__(self, out, pc, ck1, ch1, cs1, cp1, ck2, ch2, cs2, cp2, ck3, ch3, cs3, cp3, f1):
+        super(CNN3_F_P_BN, self).__init__()
+        self._fu = Fusion1dS(pc)
+        self._bn_fu = nn.BatchNorm1d(num_features=pc)
+        self.conv1 = nn.Conv1d(in_channels=pc, out_channels=ch1, kernel_size=ck1, stride=cs1, padding=cp1)
+        self.conv2 = nn.Conv1d(in_channels=ch1, out_channels=ch2, kernel_size=ck2, stride=cs2, padding=cp2)
+        self.conv3 = nn.Conv1d(in_channels=ch2, out_channels=ch3, kernel_size=ck3, stride=cs3, padding=cp3)
+        self.f1_input = math.floor((context_length - 1 + 2 * cp1 - ck1) / cs1 + 1)
+        print(self.f1_input)
+        self.f1_input = math.floor((self.f1_input + 2 * cp2 - ck2) / cs2 + 1)
+        print(self.f1_input)
+        self.f1_input = math.floor((self.f1_input + 2 * cp3 - ck3) / cs3 + 1)
+        print(self.f1_input)
+        self.f1_input *= ch3
+        self.f1_input = int(self.f1_input)
+        self.fc1 = nn.Linear(self.f1_input, f1)
+        self.fc2 = nn.Linear(f1, out)
+
+    def forward(self, x):
+        x = x.view(-1, context_length, inst_length).transpose(2,1)
+        x = F.relu(self._bn_fu(self._fu(x)))
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
         x = F.relu(self.conv3(x))
