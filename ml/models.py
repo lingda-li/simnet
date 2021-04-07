4d6c99b4e471a327656794c687354401b3292104
Thomas Flynn <tflynn@icsubmit01.sdcc.bnl.gov>
2021-04-07 13:33:07 -0400

    transformer latest

--- ml/models.py
+++ ml/models.py
@@ -2,6 +2,7 @@ import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.nn import TransformerEncoder, TransformerEncoderLayer
 from cfg import context_length, inst_length
 from enet import E1DNet
 
@@ -831,60 +832,6 @@ class CNN8_F(nn.Module):
         x = self.fc2(x)
         return x
 
-class TransformerModel(nn.Module):
-
-    def __init__(self,
-                 ntoken,  # size of vocabulary
-                 ninp,    # what dimension we want to embed our vocabulary
-                 nhead,   # number of 'heads'
-                 nhid,    # dimension of the feedforward network in nn.TransformerEncoder
-                 nlayers, #number of encoder layers
-                 dropout=0.5):
-        print("ninp",ninp,
-              "nhead",nhead,
-              "nhid",nhid,
-              "nlayers",nlayers)
-        
-        super(TransformerModel, self).__init__()
-        from torch.nn import TransformerEncoder, TransformerEncoderLayer
-        self.model_type = 'Transformer'
-        self.src_mask = None
-        self.pos_encoder = PositionalEncoding(ninp, dropout)
-        encoder_layers = TransformerEncoderLayer(ninp, nhead, nhid, dropout)
-        self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers)
-        self.encoder = nn.Embedding(ntoken, ninp)
-        self.ninp = ninp
-        self.decoder = nn.Linear(ninp, ntoken)
-
-        self.init_weights()
-
-    def _generate_square_subsequent_mask(self, sz):
-        mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
-        mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
-        return mask
-
-    def init_weights(self):
-        initrange = 0.1
-        self.encoder.weight.data.uniform_(-initrange, initrange)
-        self.decoder.bias.data.zero_()
-        self.decoder.weight.data.uniform_(-initrange, initrange)
-
-    def forward(self, src):
-        if self.src_mask is None or self.src_mask.size(0) != src.size(0):
-            device = src.device
-            mask = self._generate_square_subsequent_mask(src.size(0)).to(device)
-            print(mask.shape)
-            print(mask)
-            self.src_mask = mask
-        print("Input shape is",src.shape)
-        src = self.encoder(src) * math.sqrt(self.ninp)
-        print("Encoder output shape is",src.shape)
-        src = self.pos_encoder(src)
-        output = self.transformer_encoder(src, self.src_mask)
-        output = self.decoder(output)
-        return output
-
-
 ######################################################################
 # ``PositionalEncoding`` module injects some information about the
 # relative or absolute position of the tokens in the sequence. The
@@ -922,11 +869,12 @@ class CustomTransformerModel(nn.Module):
                  nhead, # number of 'heads'
                  nhid,  # dimension of the feedforward network in nn.TransformerEncoder
                  nlayers, #number of encoder layers
+                 nout,
                  dropout=0.5):
         super(CustomTransformerModel, self).__init__()
-        from torch.nn import TransformerEncoder, TransformerEncoderLayer
+
         self.model_type = 'Transformer'
-        self.src_mask = None
+        #self.src_mask = None
         self.pos_encoder = PositionalEncoding(ninp, dropout)
         encoder_layers = TransformerEncoderLayer(d_model=ninp,
                                                  nhead=nhead,
@@ -935,8 +883,9 @@ class CustomTransformerModel(nn.Module):
         self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers)
 
         self.ninp = ninp
-        self.decoder = nn.Linear(ninp, 2)
-
+        self.decoder = nn.Linear(ninp, nout)
+        src_mask = self._generate_square_subsequent_mask(111)
+        self.register_buffer('src_mask', src_mask)
         self.init_weights()
 
     def _generate_square_subsequent_mask(self, sz):
@@ -951,15 +900,12 @@ class CustomTransformerModel(nn.Module):
         self.decoder.weight.data.uniform_(-initrange, initrange)
 
     def forward(self, src):
+        src = src.view(-1, context_length, inst_length) #.transpose(2,1)
         src = src.transpose(0,1) # for multigpu
-        if self.src_mask is None or self.src_mask.size(0) != src.size(0):
-            device = src.device
-            mask = self._generate_square_subsequent_mask(src.size(0)).to(device)
-            self.src_mask = mask
 
         src = src * math.sqrt(self.ninp)
         src = self.pos_encoder(src)
         output = self.transformer_encoder(src, self.src_mask)
         output = self.decoder(output)
-        output = output.transpose(0,1) # for multigpu
-        return output
+
+        return output[-1]
