56bdb27d271f4eb71ad4355095d276adb6529bb3
Lingda Li <lildmh@gmail.com>
2021-05-24 11:31:53 -0400

    Add LSTM model

--- ml/models.py
+++ ml/models.py
@@ -990,3 +990,35 @@ class LLDTransformerModel(nn.Module):
         output = self.decoder(output)
 
         return output[-1]
+
+
+class InsLSTM(nn.Module):
+    def __init__(self, nout, nembed, nhidden, nlayers):
+        super(InsLSTM, self).__init__()
+
+        if nembed != inst_length:
+            self.inst_embed = nn.Linear(inst_length, nembed)
+            #self.inst_embed = nn.Sequential(
+            #    nn.Linear(inst_length, nembed)
+            #    F.relu(),
+            #)
+        else:
+            self.inst_embed = nn.Identity()
+        self.lstm = nn.LSTM(nembed, nhidden, nlayers)
+        self.linear = nn.Linear(nhidden, nout)
+
+    def init_hidden(self):
+        # type: () -> Tuple[nn.Parameter, nn.Parameter]
+        return (
+            nn.Parameter(torch.zeros(1, 1, nhidden, requires_grad=True)),
+            nn.Parameter(torch.zeros(1, 1, nhidden, requires_grad=True)),
+        )
+
+    def forward(self, src):
+        # reverse input order?
+        src = src.view(-1, context_length, inst_length)
+        x = self.inst_embed(src)
+        x = x.transpose(0, 1)
+        x, _ = self.lstm(x)
+        x = self.linear(x[-1])
+        return x
